e8b48a8e9ce227eba1e9444e266703a402e44fc3
Zoia79 <tumorxonova@gmail.com>
2024-12-01 15:26:22 +0800

    Урок 5

--- 05_lesson/05_Input.py
+++ 05_lesson/05_Input.py
@@ -1,7 +1,6 @@
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Firefox()
 driver.get("http://the-internet.herokuapp.com/inputs")
