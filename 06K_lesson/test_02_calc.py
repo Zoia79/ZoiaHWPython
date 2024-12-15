c222ba89fbb50ad1e207d7ece9b9349729a3b580
Zoia79 <tumorxonova@gmail.com>
2024-12-15 16:57:29 +0800

    Урок 6K

--- 06K_lesson/test_02_calc.py
+++ 06K_lesson/test_02_calc.py
@@ -5,23 +5,25 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
 
-driver.find_element(By.ID, 'delay').clear()
-driver.find_element(By.ID, 'delay').send_keys("45")
+def test_calc():
 
-driver.find_element(By.XPATH, "//span[text()='7']").click()
-driver.find_element(By.XPATH, "//span[text()='+']").click()
-driver.find_element(By.XPATH, "//span[text()='8']").click()
-driver.find_element(By.XPATH, "//span[text()='=']").click()
+    driver = webdriver.Chrome(
+        service=ChromeService(ChromeDriverManager().install()))
+    driver.get(
+        "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
+    driver.find_element(By.ID, 'delay').clear()
+    driver.find_element(By.ID, 'delay').send_keys("45")
+    driver.find_element(By.XPATH, "//span[text()='7']").click()
+    driver.find_element(By.XPATH, "//span[text()='+']").click()
+    driver.find_element(By.XPATH, "//span[text()='8']").click()
+    driver.find_element(By.XPATH, "//span[text()='=']").click()
 
-wait = WebDriverWait(driver, 50)
-
-result = wait.until(EC.text_to_be_present_in_element(
-    (By.CSS_SELECTOR, "div.screen"), "15"
-))
-
-assert result, f"Expected '15', but got {result}"
-driver.quit()
-
+    wait = WebDriverWait(driver, 50)
+    result = wait.until(
+        EC.text_to_be_present_in_element(
+            (By.CSS_SELECTOR, "div.screen"), "15"
+        )
+    )
+    assert result, f"Expected '15', but got {result}"
+    driver.quit()
--- 06K_lesson/test_03_shop.py
+++ 06K_lesson/test_03_shop.py
@@ -3,32 +3,37 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-driver.get("https://www.saucedemo.com/")
 
-driver.find_element(By.ID, 'user-name').send_keys("standard_user")
-driver.find_element(By.ID, 'password').send_keys("secret_sauce")
-driver.find_element(By.ID, 'login-button').click()
+def test_shop():
 
-driver.find_element(By.ID, 'add-to-cart-sauce-labs-backpack').click()
-driver.find_element(By.ID, 'add-to-cart-sauce-labs-bolt-t-shirt').click()
-driver.find_element(By.ID, 'add-to-cart-sauce-labs-onesie').click()
+    driver = webdriver.Chrome(
+        service=ChromeService(ChromeDriverManager().install()))
+    driver.get("https://www.saucedemo.com/")
 
-driver.find_element(By.CLASS_NAME , 'shopping_cart_link').click()
+    driver.find_element(By.ID, 'user-name').send_keys("standard_user")
+    driver.find_element(By.ID, 'password').send_keys("secret_sauce")
+    driver.find_element(By.ID, 'login-button').click()
 
-driver.find_element(By.ID, 'checkout').click()
+    driver.find_element(By.ID, 'add-to-cart-sauce-labs-backpack').click()
+    driver.find_element(By.ID, 'add-to-cart-sauce-labs-bolt-t-shirt').click()
+    driver.find_element(By.ID, 'add-to-cart-sauce-labs-onesie').click()
 
-driver.find_element(By.ID, 'first-name').send_keys('Zoya')
-driver.find_element(By.ID, 'last-name').send_keys('Tumorkhonova')
-driver.find_element(By.ID, 'postal-code').send_keys('670024')
+    driver.find_element(By.CLASS_NAME, 'shopping_cart_link').click()
 
-driver.find_element(By.ID, 'continue').click()
+    driver.find_element(By.ID, 'checkout').click()
 
-total_text = driver.find_element(By.CLASS_NAME, 'summary_total_label').text
+    driver.find_element(By.ID, 'first-name').send_keys('Zoya')
+    driver.find_element(By.ID, 'last-name').send_keys('Tumorkhonova')
+    driver.find_element(By.ID, 'postal-code').send_keys('670024')
 
-total = float(total_text.split(" ")[1][1:])
+    driver.find_element(By.ID, 'continue').click()
 
-expected_total = 58.29
-assert total == expected_total, f"Expected '{expected_total}', but got {total}"
+    total_text = driver.find_element(By.CLASS_NAME, 'summary_total_label').text
 
-driver.quit()
+    total = float(total_text.split(" ")[1][1:])
+
+    expected_total = 58.29
+    assert total == expected_total, \
+        f"Expected '{expected_total}', but got {total}"
+
+    driver.quit()
